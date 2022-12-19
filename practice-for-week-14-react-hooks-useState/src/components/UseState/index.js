56d0e5826364af8e88877b9ed91f4701cf9cdff9
celuu <christine.e.luu@gmail.com>
2022-12-18 17:13:48 -0800

    Updated HW

--- practice-for-week-14-react-hooks-useState/src/components/UseState/index.js
+++ practice-for-week-14-react-hooks-useState/src/components/UseState/index.js
@@ -1,7 +1,22 @@
+import './UseState.css';
+import { useState } from 'react';
+
 const UseState = () => {
+  const [theme, setTheme] = useState('light');
+  const [count, setCount] = useState(0)
+
   return (
-    <div>
+    <div className={theme}>
       <h1>UseState Component</h1>
+      <button onClick={() => setTheme("light")}>Light</button>
+      <button onClick={() => setTheme("dark")}>Dark</button>
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+        Increment
+      </button>
+      <button onClick={() => setCount((prevCount) => prevCount - 1)}>
+        Decrement
+      </button>
     </div>
   );
 };
